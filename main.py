e1f8640147b8556b7a899fe7c862b08842534c0e
Isaiah Boyd <iboyd253@gmail.com>
2021-04-20 15:05:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,93 @@
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+    # Eazy Level - Order not randomised:
+    # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+    # Hard Level - Order of characters randomised:
+    # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+    print(password_list)
+    random.shuffle(password_list)
+    print(password_list)
+
+    import math
+
+
+def paint_calc(height, width, cover):
+    total_area = height * width
+    number_of_cans = math.ceil(total_area / cover)
+    print(f" i will need {number_of_cans} cans of paint")
+
+from replit import clear
+from art import logo
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+  }
+
+
+def calculator():
+    print(logo)
+
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+      print(symbol)
+    should_continue = True
+
+while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+        num1 = answer
+    else:
+        should_continue = False
+        clear()
+        calculator()
+
+
+calculator()
